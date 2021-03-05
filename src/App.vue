3d58304b3fd1ed6fb97c2638a142cad27de425c3
¨Thomy¨ <thomybmr@gmail.com>
2021-03-05 12:28:19 -0600

    Pokedex_Parcial1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app id="fundo">
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+#fundo {
+  background-image: url("http://www.hdfondos.eu/pictures/2012/1008/1/orig_575193.jpg");
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
